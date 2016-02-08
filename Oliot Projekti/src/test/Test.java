7c44293fffea8b9ba3e9ec25689bd9f007a387fb
ville leino <villee.leino@gmail.com>
2016-02-08 12:23:06 +0200

    asd testi
    aazfaf

--- Oliot Projekti/src/test/Test.java
+++ Oliot Projekti/src/test/Test.java
@@ -8,7 +8,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		LCD.drawString("Hello!", 0, 0);
-		Delay.msDelay(500);
+		Delay.msDelay(5000);
 	}
 
 }
